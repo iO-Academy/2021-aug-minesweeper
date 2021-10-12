694cf72a0d300623ec227a57935c45ee68e04db7
Reductito <damianbell96@gmail.com>
2021-10-12 11:34:45 +0100

    merged s3 html and css, resolved merge conflicts, added default display for box, added function to generate squares to index.js

--- index.js
+++ index.js
@@ -1,48 +1,47 @@
-<<<<<<< HEAD
-// create as many divs as the number that has been inputted to the function
-// divs include the class - `grid-item`
-// the return of this function is a string (containing the html code)
-// each div should have a custom attribute `data-hit` where `1` is true and `0` is a miss
-//
-//
-// GENERAL APPROACH CURRENTLY SUGGESTED:
-// Create an array with the total number of grid items as divs,
-// use the hit box number to decide how many get the custom data attribute.
-// Once assigned then shuffle the array to randomise the order.
-// Turn the array back into a string to be returned from the function.
-//
 
-// Create an array with the total number of grid items as divs,
-// let hit = document.getElementById('hit').value
-// let total = document.getElementById('total').value
-// let miss = document.getElementById('total').value - document.getElementById('hit').value
+//removes grid container from document flow
+document.getElementById('gridContainer').style.display="none"
 
-//require index.html at top of page
 //function to generate all game tiles
-let generateSquares = () => {
-    //create empty array
-    let squares = []
-    // replace 10. for every hit we are putting a div into the array with data-hit 1
-    for(i=0; i<6; i++) {
-        squares.push('<div class="grid-item" data-hit="1"></div>')
+document.querySelector('.form').addEventListener('submit', (e) => {
+    //Defining total grid size, number of lynx and misses from user input
+    let numLynx = document.getElementById('numLynx').value
+    let gridSize = document.getElementById('gridSize').value
+    let miss = gridSize - document.getElementById('numLynx').value
+    //stops form from submitting
+    e.preventDefault()
+    //sets the grid container display to default
+    document.getElementById('gridContainer').style.display=""
+    let generateSquares = () => {
+        //create empty array
+        let squares = []
+        // replace 10. for every hit we are putting a div into the array with data-hit 1
+        for(i=0; i<numLynx; i++) {
+            squares.push('<div class="grid-item" data-hit="1"></div>')
+        }
+     // replace 12. for every hit we are putting a div into the array with data-hit 0
+        for(i=0; i<miss; i++) {
+            squares.push('<div class="grid-item" data-hit="0" ></div>')
+        }
+        //shuffle the array
+        let shuffle = (array) => array.map(a => ({ sort: Math.random(), value: a })).sort((a, b) => a.sort - b.sort).map(a => a.value)
+        let shuffledArr = shuffle(squares)
+        console.log(squares)
+        //create empty string
+        emptyString = ''
+        //adding each
+        shuffledArr.forEach((square) => {
+            document.getElementById('gridContainer').innerHTML += square
+        })
     }
- // replace 12. for every hit we are putting a div into the array with data-hit 0
-    for(i=0; i<6; i++) {
-        squares.push('<div class="grid-item" data-hit="0" ></div>')
-    }
-    //shuffle the array
-    let shuffle = (array) => array.map(a => ({ sort: Math.random(), value: a })).sort((a, b) => a.sort - b.sort).map(a => a.value)
-    let shuffledArr = shuffle(squares)
-    //create empty string
-    emptyString = ''
-    //adding each 
-    shuffledArr.forEach((square) => {
-        document.getElementById('gridContainer').innerHTML += square
-    })
-}
+    //execute function
+    generateSquares()
+    //scroll to top of game play area
+    document.querySelector('#gridContainer').scrollIntoView({
+        behavior: 'smooth'
+    });
+})
 
-generateSquares()
-=======
 
 // DOCBLOCK READ PLS
 // Function takes 2 integer inputs from form.
@@ -72,7 +71,3 @@ function validateInput(num1, num2){
 
     return true
 }
-
-validateInput(10, -6.5)
-console.log(errorMessage)
->>>>>>> s3-generateGrid
