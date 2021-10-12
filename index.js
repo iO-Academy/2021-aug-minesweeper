ecfc86fd854c32e1cec6d1f4ac1a69b0b9b78dee
DevUsman-18 <mohammad_usman117@hotmail.co.uk>
2021-10-12 09:28:00 +0100

    updated with boolean responses and global error message

--- index.js
+++ index.js
@@ -5,17 +5,23 @@
 //Function checks if number is integer,
 // if positive
 // and if hitboxes (num2) does not exceed half of the grid size (num1).
+// global error message is populated upon error, dictated by the conditional, ready to be displayed
 //return true if no errors
+
+let errorMessage = ""
 
 function validateInput(num1, num2){
     if (!Number.isInteger(num1) || !Number.isInteger(num2)){
-        return 'Please only input whole numbers'
+        errorMessage = 'Please only input whole numbers'
+        return false
     }
     if (Math.sign(num1) === -1 || Math.sign(num2) === -1) {
-        return 'You must have a positive grid size and number of lynx to find'
+        errorMessage = 'You must have a positive grid size and number of lynx to find'
+        return false
     }
     if ((num1 / 2) < num2) {
-        return 'Number of lynx can not exceed half of the number you picked for the Forest size'
+        errorMessage = 'Number of lynx can not exceed half of the number you picked for the Forest size'
+        return false
     }
 
     return true
