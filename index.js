d4aea76779ef91b59431679518a691bb62a9a154
kimberley <kim.dobney@yahoo.co.uk>
2021-10-12 10:35:14 +0100

    made cards appear on the screen and added translucent background

--- index.js
+++ index.js
@@ -22,21 +22,21 @@ let generateSquares = () => {
     //create empty array
     let squares = []
     // replace 10. for every hit we are putting a div into the array with data-hit 1
-    for(i=0; i<10; i++) {
-        squares.push('<div class="grid-item" data-hit="1">1</div>')
+    for(i=0; i<6; i++) {
+        squares.push('<div class="grid-item" data-hit="1"></div>')
     }
  // replace 12. for every hit we are putting a div into the array with data-hit 0
-    for(i=0; i<12; i++) {
-        squares.push('<div class="grid-item" data-hit="0" >0</div>')
+    for(i=0; i<6; i++) {
+        squares.push('<div class="grid-item" data-hit="0" ></div>')
     }
     //shuffle the array
-    let shuffle = () => squares.map(a => ({ sort: Math.random(), value: a })).sort((a, b) => a.sort - b.sort).map(a => a.value)
-    let shuffledArr = shuffle()
+    let shuffle = (array) => array.map(a => ({ sort: Math.random(), value: a })).sort((a, b) => a.sort - b.sort).map(a => a.value)
+    let shuffledArr = shuffle(squares)
     //create empty string
     emptyString = ''
     //adding each 
     shuffledArr.forEach((square) => {
-        document.getElementById('container').innerHTML += square
+        document.getElementById('gridContainer').innerHTML += square
     })
 }
 
